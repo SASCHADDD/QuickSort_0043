3ee2ae07e25fc577c248c2e4ddd8a1ed43f86674
SASCHADDD <saschadanu23@gmail.com>
2024-03-28 16:47:10 +0700

    commit 3

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -9,7 +9,6 @@ int n;
 
 void input(){
     while (true)
-}
     {
         cout << "Masukan panjang element array: ";
         cin >> n;
@@ -19,3 +18,15 @@ void input(){
         else 
             cout <<"\nMaksimum panjang array adalah 20" <<endl;
     }
+
+    cout << "\n-------------------" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n-------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i]
+    }
+    
+}
