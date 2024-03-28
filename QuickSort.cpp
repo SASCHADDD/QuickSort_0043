b6744547bf58fd10c44c7571e387be1da00a69ee
SASCHADDD <saschadanu23@gmail.com>
2024-03-28 17:12:41 +0700

    step 12

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -79,3 +79,6 @@ if (low < j){ //step 11
     // swap the pivot element with the element at index j
     swap(low, j);
 }
+
+// recursive call to sort the left sub array
+q_short(low, j - 1); //step 12
